fb2afac5a754c7e7d3af943f44a005cace49bd64
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2022-03-22 22:22:24 -0300

    test segment  + mixpanel

--- packages/use-track/src/App.js
+++ packages/use-track/src/App.js
@@ -1,28 +1,38 @@
-import { useTrack } from "./helper/Instrumentalization";
+import { useSegment } from "./helper/Instrumentalization";
 
 function App() {
-  const track = useTrack();
+  const segment = useSegment();
 
   return (
     <div className="App">
       <header className="App-header">
         <a
           onClick={(e) => {
-            track("Hi Segment, another custom hook", {
-              account_id: 1,
-              distinct_id: "goku@dbz.com",
-              name: "Goku",
+            segment.track("Hi!!", {
+              id: 3,
+              email: "gohan@dbz.com",
+              name: "Gohan",
             });
-            track("Hi Segment, another custom hook", {
-              account_id: 2,
-              distinct_id: "saitama@punch.com",
-              name: "Saitama",
-            });
+            // segment.track("Hi Segment, email reference", {
+            //   account_id: 3,
+            //   email: "vegeta@capsulacorp.com",
+            //   food: "MEAT",
+            // });
+            // segment.group("123", {
+            //   company_id: 2,
+            //   name: "Hero Onepunch",
+            //   industry: "Monster Hunter",
+            // });
+            // segment.identify("jiraiya-sensei", {
+            //   name: "Jiraiya",
+            //   email: "jiraiya@frog.com",
+            //   hobby: "Sleep",
+            // });
           }}
           className="App-link"
           href="#"
         >
-          Track
+          Send event to segment
         </a>
       </header>
     </div>
@@ -31,24 +41,7 @@ function App() {
 
 export default App;
 
-//segment-token: yyrJ2y9wvh5IsuSRSn7z-fzjiwOlFlUXq0Ohnt0h8DE.9CGO3XcP44EtspUNoao-AcZ2wyM734an0JiqQDRvFow
-
-// {
-//   "name": "workspaces/arthur-valentim-kasper/sources/js",
-//   "parent": "workspaces/arthur-valentim-kasper",
-//   "display_name": "js",
-//   "write_keys": [
-//    "2qexT5tmXGmTbVw0Rs75hLnPueW7GEB6"
-//   ],
-//   "library_config": {
-//    "metrics_enabled": false,
-//    "retry_queue": false,
-//    "cross_domain_id_enabled": false,
-//    "api_host": ""
-//   },
-//   "labels": {},
-//   "id": "6fvQdVDGlF",
-//   "function_config": [],
-//   "create_time": "2022-02-18T01:06:37Z",
-//   "catalog_name": "catalog/sources/javascript"
-// }
+/**
+ * Quando uma vez for executado o identify, é gerado um cookie e localstorage 
+ * que reutiliza as infos para atrelar os próximos evios ao usuário existente no cookie.
+ */
--- packages/use-track/src/helper/Instrumentalization.js
+++ packages/use-track/src/helper/Instrumentalization.js
@@ -3,22 +3,23 @@ import { AnalyticsBrowser } from "@segment/analytics-next";
 
 const writeKey = "2qexT5tmXGmTbVw0Rs75hLnPueW7GEB6";
 
-const handleSegment = () => {
-  const loadAnalytics = async () => {
-    let [response] = await AnalyticsBrowser.load({ writeKey });
-    setAnalytics(response);
-  };
-  loadAnalytics();
-};
-
-export const useTrack = () => {
+export const useSegment = () => {
   const [analytics, setAnalytics] = useState();
+
+  const handleSegment = () => {
+    const loadAnalytics = async () => {
+      let [response] = await AnalyticsBrowser.load({ writeKey });
+      setAnalytics(response);
+    };
+    loadAnalytics();
+  };
 
   useEffect(() => {
     handleSegment();
   }, [writeKey]);
 
-  return analytics?.track;
+  //return analytics?.track;
+  return analytics;
 };
 
 //export default useTrack;
